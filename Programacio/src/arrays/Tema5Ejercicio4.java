084dbb9ff6db3c984c9a47dd24ee35490535af61
Reti35 <reneribera3@gmail.com>
2021-04-13 13:20:02 +0200

    moar

--- Programacio/src/arrays/Tema5Ejercicio4.java
+++ Programacio/src/arrays/Tema5Ejercicio4.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package arrays;
 
 import java.util.Scanner;
@@ -20,7 +20,6 @@ public class Tema5Ejercicio4 {
 			if (num[i] > 0) {
 
 				pos++;
-				
 
 			} else if (num[i] < 0) {
 
@@ -45,48 +44,3 @@ public class Tema5Ejercicio4 {
 	}
 
 }
-=======
-import java.util.Scanner;
-
-public class Tema5Ejercicio4 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int[] num = new int[10];
-		int pos = 0, neg = 0, cer = 0;
-
-		System.out.println("Introduce 10 numeros");
-
-		for (int i = 0; i < num.length; i++) {
-
-			num[i] = sc.nextInt();
-
-			if (num[i] > 0) {
-
-				pos++;
-
-			} else if (num[i] < 0) {
-
-				neg++;
-
-			} else {
-
-				cer++;
-
-			}
-
-		}
-
-		System.out.println();
-		System.out.println("Entre los numeros introducidos hay:");
-		System.out.println("Positivos: " + pos);
-		System.out.println("Negativos: " + neg);
-		System.out.println("Ceros: " + cer);
-
-		sc.close();
-
-	}
-
-}
->>>>>>> b1c8fa4e260d76e6aae2b743d1a474941c735aed
